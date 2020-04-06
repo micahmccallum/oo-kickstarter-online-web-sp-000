2938fd6d7fd2f4651bc88b64b87ccedfc4a8ca7e
Micah McCallum <micahmccallum@outlook.com>
2020-04-06 05:37:01 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,15 @@
+require 'pry'
+class Project
+  attr_reader :backers, :title
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers.push(backer)
+    backer.backed_projects.push(self)
+
+  end
+end
